280990efc8c9cb59fac0f760a6ea660750193425
Allen <allensoberano@gmail.com>
2021-10-26 15:46:50 -0700

    Create Essential Feed Framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Allen Soberano on 10/26/21.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
