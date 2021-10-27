79b98ccf8d57f864066ec7dfcf30ae841744ad57
Allen <allensoberano@gmail.com>
2021-10-27 09:16:23 -0700

    moved remote feed loader to production

---
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,27 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Allen Soberano on 10/27/21.
+//
+
+import Foundation
+
+public protocol HTTPClient{
+	func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+
+	private let url: URL
+	private let client: HTTPClient
+
+	public init(url: URL, client: HTTPClient){
+		self.client = client
+		self.url = url
+	}
+
+	public func load(){
+		client.get(from: url)
+	}
+}
--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -6,24 +6,7 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-	let client: HTTPClient
-	let url: URL
-
-	init(url: URL, client: HTTPClient){
-		self.client = client
-		self.url = url
-	}
-
-	func load(){
-		client.get(from: url)
-	}
-}
-
-protocol HTTPClient{
-	func get(from url: URL)
-}
+import EssentialFeed
 
 class RemoteFeedLoaderTests: XCTestCase {
 
@@ -51,13 +34,11 @@ class RemoteFeedLoaderTests: XCTestCase {
 	}
 
 	private class HTTPClientSpy: HTTPClient {
-		
+
 		var requestedURL: URL?
 
 		func get(from url: URL){
 			requestedURL = url
 		}
-
-
 	}
 }
